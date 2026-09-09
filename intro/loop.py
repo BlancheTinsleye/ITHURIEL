05e86f9edd678cc1789ad6b76c2751b7fa3e89ea
BlancheTinsleye <bjashleymercado@gmail.com>
2026-09-09 09:16:23 +0800

    loop

--- intro/loop.py
+++ intro/loop.py
@@ -1,35 +1,40 @@
-# letters = ["a", "c", "g"]
-# length = len(letters)
+# we can make list and lists can have multiple elements (data types)
+# uncomment the examples to see the output
+letters = ["a", "c", "g"]
+# len is how many or the total count of elements in a list
+length = len(letters)
 
-# for i in range(length):
-#     print(i)
+# in range means you loop up to a specific number
+for i in range(length):
+    print(i)
 
-# for letter in letters:
-#     print(letter)
-
+# you can use the indexing to get a character from a string
+# the count starts with 0 so the total count is off by 1
+# and using a negative number will make you start from the
+# end. 
 # print(letters[-3])
 
-# txt = 4
+txt = "Hello"
 
-# for lttr in range(txt):
-#     print(lttr)
+# you can also print each letter of the string by using
+# a for loop
+for lttr in range(len(txt)):
+    print(lttr)
 
-# print(letters)
+for letter in letters:
+    print(letter)
 
+# fruits is a list the contain strings
 fruits = ["apple", "mango", "banana", "grape", "orange"]
 
-# print(fruits[3])
+# print a specifi fruit
+print(fruits[3])
 
-# for frt in fruits:
-#     if frt == "banana":
-#         # break
-#         print("Minion")
-#     else:
-#         print(frt)
-#     print("\n")
-
-
-    # print(frt)
-
-#     print(fruits[0])
-
+# print the fruits with some tweaking
+for frt in fruits:
+    if frt == "banana":
+        # break
+        print("Minion")
+    else:
+        print(frt)
+    print("\n")
