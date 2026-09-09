314d3f87b26be7980ddced07e3b9c3050822d038
BlancheTinsleye <bjashleymercado@gmail.com>
2026-09-09 08:47:35 +0800

    intro cleaned

--- intro/intro.py
+++ intro/intro.py
@@ -1,22 +1,24 @@
-lowest = max(12, 11)
+# A variable can contain different data types
+var_int = 12
+var_str = "Hi"
+var_numstr = "12" # a number can be a string if inside a "" or ''
+var_float = 2.00
+var_bool = True # can be False
+var_null = None # nothing
 
-# print(lowest)
+# to show a string or simply print
+print(var_int) # output: 12
 
-text = "HELLO"
+# you can also print a bit more
+print("I am me, " + var_str) # but you cant concatinate a string and an int
+# print("I am " + var_int + " years old") # uncomment the print to see the error
 
-# print(text.lower())
+# you can print an output of a computation
+print(var_int + var_float)
 
-# print(text.upper())
+# print bool 
+print(var_bool)
 
-number = None
-string = "5"
-
-# print(number + string)
-
-colors = ["blue", "red", number, string, text]
-
-# []
-
-print(colors)
-
+# print null and the output is none
+print(var_null)
 
